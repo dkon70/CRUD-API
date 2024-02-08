3cd80a27454ac99e3e99bad161c7733343960a1b
dkon70 <dkon70@inbox.ru>
2024-02-09 02:36:03 +0700

    feat: set up project, add basic eslint, ts and prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: false,
+    es2021: true,
+    node: true,
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+}
